6ea4c19f49322a107850584ac266ff1d14dd82df
Keil1201 <101579075+Keil1201@users.noreply.github.com>
2023-10-26 20:05:26 -0400

    Update NmsProject.py

--- NmsProject.py
+++ NmsProject.py
@@ -29,14 +29,13 @@ def fileSelectWindow():
         initialdir='/',
         filetypes=filetypes)
     with file as file:
-        ipList=file.read()
-        ipList= ipList.splitlines()
+        ipList = file.read()
+        ipList = ipList.splitlines()
 
         for ip in ipList:
             #creates list to store ping ouput
             response = os.popen(f"ping -n 1 {ip} ").read()
             myList.append(response)
-            print(response)
 
     for x in myList:
         ip = x[9:17]
@@ -44,8 +43,10 @@ def fileSelectWindow():
         IPlabels.append(customtkinter.CTkLabel(frame, text=ip))
         IPlabels[y].place(x=45, y=50+(y*30))
     for x in myList:
-        latency = x[75:79]
-        y=myList.index(x)
+        latency = x[75:78]
+        y = myList.index(x)
+        if ("Request timed out." or "unreachable") in x:
+            latency = "Down"
         latencyLabels.append(customtkinter.CTkLabel(frame, text=latency))
         latencyLabels[y].place(x=215, y=50 + (y * 30))
     for x in myList:
@@ -53,13 +54,21 @@ def fileSelectWindow():
             status = "Down"
             y = myList.index(x)
             statusLabels.append(customtkinter.CTkLabel(frame, text=status))
-            statusLabels[y].place(x=320, y=50 + (y * 30))
+            statusLabels[y].place(x=318, y=50 + (y * 30))
+            secWindow()
         else:
             status = "Up"
             y = myList.index(x)
             statusLabels.append(customtkinter.CTkLabel(frame, text=status))
             statusLabels[y].place(x=320, y=50 + (y * 30))
 
+
+def secWindow():
+    secondaryWindow = customtkinter.CTk()
+    secondaryWindow.geometry('500x100')
+    close = customtkinter.CTkButton(secondaryWindow, text='Okay', command=secondaryWindow.destroy)
+    close.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    secondaryWindow.mainloop()
 
 
 #Set window width and height
@@ -69,8 +78,7 @@ frame.pack(padx=50, pady=50)
 
 #Create buttons
 selectFile = customtkinter.CTkButton(mainWindow, text='Select File', font=("Inter", 10), command = fileSelectWindow)
-#loadIp = customtkinter.CTkButton(mainWindow, text='load', font=("Inter", 10), command = loadIps)
-title = customtkinter.CTkLabel(mainWindow, text = 'Network Monitoring Systems', corner_radius= 10, fg_color= 'grey', text_color='white')
+title = customtkinter.CTkLabel(mainWindow, text = 'Network Monitoring System', corner_radius= 10, fg_color= 'grey', text_color='white')
 ipLabel = customtkinter.CTkLabel(master = frame, text = 'IP', corner_radius= 10, fg_color= 'grey', text_color='white')
 latencyLabel = customtkinter.CTkLabel(master= frame, text = 'Latency', corner_radius= 10, fg_color= 'grey', text_color='white')
 statusLabel = customtkinter.CTkLabel(master=frame, text = 'Status', corner_radius= 10, fg_color= 'grey', text_color='white')
@@ -79,7 +87,7 @@ statusLabel = customtkinter.CTkLabel(master=frame, text = 'Status', corner_radiu
 selectFile.place(x=175, y=450)
 title.place(x=165, y=15)
 ipLabel.place(x=50, y=15)
-latencyLabel.place(x=200, y = 15)
+latencyLabel.place(x=200, y=15)
 statusLabel.place(x=300, y=15)
 
 
